f1d20e4ca45b950079f4429b7368b565e35bb974
divacs <sonja.divac@yahoo.com>
2024-11-29 15:06:48 +0100

    data context

---
+++ WorkshopPro/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkshopPro.Model;
+
+namespace WorkshopPro.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Workshop> Workshops { get; set; }
+        public DbSet<ProjectMaterial> ProjectMaterials { get; set; }
+    }
+}
